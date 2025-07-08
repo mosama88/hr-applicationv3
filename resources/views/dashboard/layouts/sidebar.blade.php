4fd9f1a0d09d1a8efecd33d2d64a2df036824470
mosama <mosama88@mail.com>
2025-07-08 12:49:36 +0300

    Done

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
         <!--begin::Sidebar Brand-->
         <div class="sidebar-brand">
             <!--begin::Brand Link-->
-            <a href="./index.html" class="brand-link">
+            <a href="{{ route('dashboard.index') }}" class="brand-link">
                 <!--begin::Brand Image-->
                 <img src="{{ asset('dashboard') }}/assets/dist/assets/img/AdminLTELogo.png" alt="AdminLTE Logo"
                     class="brand-image opacity-75 shadow" />
